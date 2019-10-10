f6ac7330c41b9b305ebf82cfffcc1c1eeb33c51d
John Dupuis <wasp7@Johns-MacBook-Pro.local>
2019-10-10 14:46:10 -0400

    added thunk middleware for the api call

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
 import App from './App'
 import selectionReducer from './reducers/selectionReducer'
 
 
-const store = createStore(selectionReducer)
+const store = createStore(
+  selectionReducer,
+  applyMiddleware(thunk)
+)
 
 ReactDOM.render(
   (<Provider store={store}>
